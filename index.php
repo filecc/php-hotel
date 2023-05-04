90c15f7541f81854f84a38c41b9f1088f5db9752
filecc <fil.gia@live.it>
2023-05-04 14:53:19 +0200

    filter for parking and vote

--- index.php
+++ index.php
@@ -46,7 +46,38 @@ $hotels = [
 ];
 
 $filteredHotels = [];
-$filteredHotels = $hotels;
+$noResults = false;
+if ((empty($_GET['vote']) || $_GET['vote'] == 'all') && empty($_GET['park'])){
+    $filteredHotels = $hotels;
+} else {
+    $requiredVote = $_GET['vote'];
+    if ($requiredVote == 'all' && !empty($_GET['park'])){
+        foreach ($hotels as $value) {
+            if ($value['parking'] == true){
+                $filteredHotels[] = $value;
+            };
+        }
+    } elseif ($requiredVote != 'all' && !empty($_GET['park'])) {
+        foreach ($hotels as $value) {
+            if ($value['parking'] == true && $value['vote'] == $requiredVote){
+                $filteredHotels[] = $value;
+            };
+        }
+    } elseif ($requiredVote != 'all'){
+        foreach ($hotels as $value) {
+            if ($value['vote'] == $requiredVote){
+                $filteredHotels[] = $value;
+            };
+        }
+    }
+
+    if (count($filteredHotels) === 0){
+        $noResults = true;
+    };
+};
+
+
+
 ?>
 
 
@@ -62,9 +93,32 @@ $filteredHotels = $hotels;
     <link rel="stylesheet" href="./css/master.css">
   </head>
   <body class="container">
-    <h1 class="text-center">Hotels</h1>
+    <h1 class="text-center py-">Hotels</h1>
+    <div class="container py-3">
+        <form action="<?php $_SERVER['PHP_SELF'] ?>" method="GET" class="d-flex justify-content-between">
+            <div>
+                    <span>Parcheggio: </span>
+                    <input type="checkbox" class="me-3" name="park">
+                    <span>Voto: </span>
+                    <select name="vote" id="vote">
+                        <option value="all">Tutti</option>
+                        <option value="1">1 stella</option>
+                        <option value="2">2 stelle</option>
+                        <option value="3">3 stella</option>
+                        <option value="4">4 stella</option>
+                        <option value="5">5 stella</option>
+                    </select>
+            </div>
+                
+                <button type="submit">Cerca</button>
+        </form>
+    </div>
+       
     <div class="container">
         <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3">
+                <?php if($noResults): ?>
+                    <p class="text-center w-100">Nessun risultato trovato per la tua ricerca.</p>
+                <?php else:?>
                 <?php foreach ($filteredHotels as $value) { ?>
             <div class="col mb-3">
                     <div class="card">
@@ -72,7 +126,7 @@ $filteredHotels = $hotels;
                         <div class="card-body">
                             <h5 class="card-title"><?php echo $value['name']; ?></h5>
                             <p class="card-text"><?php echo $value['description']; ?></p>
-                            <?php for($i=0;$i<5;$i++){?>
+                            <?php for($i=1;$i<=5;$i++){?>
                                 <?php if($i <= $value['vote']): ?>
                                     <i class="bi bi-star-fill text-warning"></i>
                                 <?php else:?>
@@ -94,6 +148,7 @@ $filteredHotels = $hotels;
             </div>
                 <?php }?>
         </div>
+                <?php endif?>
     </div>
    
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
